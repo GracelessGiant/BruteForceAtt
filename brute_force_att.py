473955f14a4acac5948d3842c2509960dabd74b6
GracelessGiant <marchionepat43@gmail.com>
2020-06-23 08:47:50 -0400

    Combined all the lists into one and trying to decipher a way to create a loop

--- brute_force_att.py
+++ brute_force_att.py
@@ -5,13 +5,11 @@ Basic program to attempt to guess the password for an account
 """
 import time
 
-low_alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "v",
-                "w", "x", "y", "z"]
-cap_alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V",
-                "W", "X", "Y", "Z"]
-special_char = [" ", "!", '"', "#", "$", "%", "&", "'",	"(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">",
-                "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
-numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "v",
+              "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+              "R", "S", "T", "V", "W", "X", "Y", "Z", " ", "!", '"', "#", "$", "%", "&", "'",	"(", ")", "*", "+", ",",
+              "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "0", "1",
+              "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
 def brute_att(password):
@@ -19,15 +17,21 @@ def brute_att(password):
     Goes through the process to attempt to crack the password
     :param password: the entered password
     """
-    pass
+    guess = ""
 
+    if password in characters:      # checks if the password is of length 1
+        return 0
+
+    for x in range(15):             # checks all possibility of length 15 or less
+        
 
 
 def main():
     password = input("Enter your password: ")
 
     start = time.time()
-    brute_att(password)
+    answer = brute_att(password)
+    print(answer)
     overall_time = time.time() - start
 
     print("Your password "+password+" was cracked in "+str(overall_time)+" seconds.")
